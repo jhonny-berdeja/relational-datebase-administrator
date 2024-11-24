7896d560104b4b6e3295580208ac4fd49753ca7d
jhonny-berdeja <jhonny.berdeja.100@gmail.com>
2024-11-24 03:09:51 -0300

    first commit

---
+++ src/main/java/com/jberdeja/relational_database_administrator/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.jberdeja.relational_database_administrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jberdeja/relational_database_administrator/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jberdeja.relational_database_administrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
